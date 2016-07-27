46b15e68b0bdf5bdea4df9a41a073bf6f5a8ce70
Rod <r0dvan@MacBook-Pro-de-Rod.local>
2016-07-27 00:33:08 -0500

    cambios comments

--- app/comments/src/Jobs/FetchComments.php
+++ app/comments/src/Jobs/FetchComments.php
@@ -1,13 +1,6 @@
 <?php
 
-/**
- * This file is part of Ajax Comment System for Laravel™.
- *
- * (c) HazzardWeb <hazzardweb@gmail.com>
- *
- * For the full copyright and license information, please visit:
- * http://codecanyon.net/licenses/standard
- */
+
 
 namespace Hazzard\Comments\Jobs;
 
